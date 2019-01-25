8df595a7c1b4fa7db43882206083fe6b7fc06621
herongxhr <herongxhr@163.com>
2019-01-25 20:15:03 +0800

    试用knob addon

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,2 +1,6 @@
+//插件导入的顺序很重要，
+//导入顺序决定了它们在插件面板中的顺序
 import '@storybook/addon-actions/register';
+import '@storybook/addon-knobs/register';
 import '@storybook/addon-links/register';
+ 
--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,12 @@
 import { configure } from '@storybook/react';
-import requireContext from 'require-context.macro';
+import 'storybook-chromatic';
+
 import '../src/index.css';
-const req = requireContext('../src/components', true, /\.stories\.js$/);
+
+const req = require.context('../src/components', true, /\.stories\.js$/);
+
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
 configure(loadStories, module);
